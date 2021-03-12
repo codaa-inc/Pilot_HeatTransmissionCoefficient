3b9e37deaf9c40f074b87ae8a733205c89e85d9b
EunJin Lee <didvud0323@gmail.com>
2021-03-12 18:09:10 +0900

    update page layout, build api function

--- HeatTransCo/HeatTransCo/urls.py
+++ HeatTransCo/HeatTransCo/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from service.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', main)
+    path('', main),
+    path('data/', data),
 ]
 
--- HeatTransCo/service/views.py
+++ HeatTransCo/service/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+import json
+import os
+from django.http import HttpResponse
+
 # Create your views here.
 def main(request) :
     return render(request, 'index.html')
+
+def data(request) :
+    print(os.path.realpath(__file__))
+    print(os.path.abspath(__file__))
+    file = open('data/heatTransCoData.json')
+    print(file)
+    with open('data/heatTransCoData.json', 'r') as f:
+        json_file = json.load(f)
+        print(json_file)
+    return HttpResponse(json.dumps(json_file), content_type="application/json")
